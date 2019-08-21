f72bcabba82447dacd2c6ba95f5bfc5b5b7a4516
Dzmitry Tsebruk <mikhama@tut.by>
2019-08-21 11:06:04 +0300

    fix: fix port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const db = require('./config/db');
 const routes = require('./app/routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
